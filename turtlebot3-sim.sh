e08b36ffdc764980ead44eb34ae60cb36dd04409
wn1980 <wn1980@hotmail.com>
2022-01-25 13:04:05 +0000

    update

--- turtlebot3-sim.sh
+++ turtlebot3-sim.sh
@@ -4,8 +4,11 @@ set -e
 
 sudo apt update && \
 sudo apt install -y \
+    ros-${ROS_DISTRO}-slam-toolbox \
+    ros-${ROS_DISTRO}-nav2-bringup \
     ros-${ROS_DISTRO}-turtlebot3-simulations \
     ros-${ROS_DISTRO}-turtlebot3-teleop \
+    ros-${ROS_DISTRO}-turtlebot3-navigation2 \
     && echo "DONE"
 
 echo "export TURTLEBOT3_MODEL=waffle_pi" >> ~/.bashrc
